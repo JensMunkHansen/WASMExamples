cacd4b112bfb20c5cf961be100ba072510d87b9d
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2024-08-07 20:54:52 +0200

    VSCode debugger works

---
+++ VSCodeDebug/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cmake --preset default -B build
+cmake --build build --preset Debug
--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
     echo "Emscripten"
     source "$HOME/github/emsdk/emsdk_env.sh"
     emcmake cmake -S. -Bbuild -DVTK_DIR=/home/jmh/github/VTKCMake/install/lib/cmake/vtk
-    cmake --build build
+    cmake --build build --verbose
 )
 
 
