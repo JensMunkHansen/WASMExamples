0ad2aa81cb164956a13bcb4fcf9279a024ac7918
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2024-11-05 21:42:18 +0100

    ..

--- Threads/example.c
+++ Threads/example.c
@@ -1,46 +1,80 @@
 #include <pthread.h>
 #include <stdio.h>
-#include <unistd.h>
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
-int thread_started = 0;
+int thread1_started = 0;
+int thread2_started = 0;
 
-void* thread_callback(void* arg)
+void* worker1(void* arg)
 {
   int* val = (int*)arg;
 
-  // Signal that the worker thread has started
+  // Signal that thread 1 has started
   pthread_mutex_lock(&mutex);
-  thread_started = 1;
+  thread1_started = 1;
   pthread_cond_signal(&cond);
   pthread_mutex_unlock(&mutex);
 
+  // Do some work with synchronized access to shared variable
+  pthread_mutex_lock(&mutex);
   *val += 1;
-  printf("Worker thread: incremented value to %d\n", *val);
+  printf("Worker 1: incremented value to %d\n", *val);
+  pthread_mutex_unlock(&mutex);
+
   return NULL;
 }
 
-// emrun example.html (works, emrun --no_browser --port 8080 .
+void* worker2(void* arg)
+{
+  int* val = (int*)arg;
+
+  // Signal that thread 2 has started
+  pthread_mutex_lock(&mutex);
+  thread2_started = 1;
+  pthread_cond_signal(&cond);
+  pthread_mutex_unlock(&mutex);
+
+  // Do some work with synchronized access to shared variable
+  pthread_mutex_lock(&mutex);
+  *val += 2;
+  printf("Worker 2: incremented value to %d\n", *val);
+  pthread_mutex_unlock(&mutex);
+
+  return NULL;
+}
+
 int main()
 {
-  puts("Before the thread");
+  pthread_t thread1, thread2;
+  int value = 0;
 
-  pthread_t thread_id;
-  int arg = 42;
-  pthread_create(&thread_id, NULL, thread_callback, &arg);
+  printf("Main thread: initial value is %d\n", value);
 
-  // Wait until the worker thread has started
+  // Create worker threads
+  if (pthread_create(&thread1, NULL, worker1, &value) != 0)
+  {
+    printf("Error creating thread 1\n");
+    return 1;
+  }
+  if (pthread_create(&thread2, NULL, worker2, &value) != 0)
+  {
+    printf("Error creating thread 2\n");
+    return 1;
+  }
+
+  // Wait until both threads have started
   pthread_mutex_lock(&mutex);
-  while (!thread_started)
+  while (!thread1_started || !thread2_started)
   {
     pthread_cond_wait(&cond, &mutex);
   }
   pthread_mutex_unlock(&mutex);
 
-  pthread_join(thread_id, NULL);
+  // Join both worker threads
+  pthread_join(thread1, NULL);
+  pthread_join(thread2, NULL);
 
-  puts("After the thread");
-
+  printf("Main thread: final value is %d\n", value);
   return 0;
 }
