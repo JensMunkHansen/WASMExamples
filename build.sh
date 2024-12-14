a8baf7d9cdc5bb88bdec4f2115ad7ee465b80426
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2024-12-14 14:04:58 +0100

    ..

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-cd $(pwd)/SideModule/ES6
+cur_dir=$(pwd)
+cd $cur_dir/SideModule/ES6
 source build.sh
+
+cd $cur_dir/Threading
+source build.sh wasm
+rm $cur_dir/Threading/build_wasm
+source build.sh wasm threading
+
--- clean.sh
+++ clean.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 rm -Rf $(pwd)/SideModule/ES6/build
+rm -Rf $(pwd)/Threading/build_wasm
