fe0d106fc507452ebb2e8642f554d4b29f364161
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2025-01-11 20:27:13 +0100

    ..

---
+++ VSCodeDebug/ES6/launch_browser.sh
@@ -0,0 +1,8 @@
+/usr/bin/google-chrome-stable \
+  --user-data-dir=/tmp/chrome_dev \
+  --allow-file-access-from-files \
+  --disable-web-security \
+  --remote-debugging-port=9222 \
+  --auto-open-devtools-for-tabs \
+  --no-first-run \
+  --no-default-browser-check
