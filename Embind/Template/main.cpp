a664c9725172619b9ee6edc97167ad49b3587da6
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2025-01-21 19:54:52 +0100

    sanitize

--- Embind/Template/main.cpp
+++ Embind/Template/main.cpp
@@ -1,24 +1,43 @@
 #include "emscripten/bind.h"
+#include <iostream>
 #include <vector>
 
-template <typename FloatType> class TemplateClass {
+template <typename FloatType>
+class TemplateClass
+{
 public:
-  TemplateClass() : Data(0), VectorData(1) {
+  TemplateClass()
+    : Data(0)
+    , VectorData(1)
+  {
   }
   FloatType Data;
   std::vector<FloatType> VectorData;
-  void setData(FloatType x) { Data = x;}
-  FloatType getData() const {return Data;}
+  void setData(FloatType x)
+  {
+    Data = x;
+#ifdef __SANITIZE_ADDRESS__
+    int* ptr = new int(42); // Dynamically allocate memory.
+    delete ptr;             // Free the memory.
+    // Use after free: Access the pointer after deleting it.
+    std::cout << "Use after free value: " << *ptr << std::endl;
+#endif
+  }
+  FloatType getData() const
+  {
+    return Data;
+  }
 };
 
-EMSCRIPTEN_BINDINGS(TemplateClass) {
+EMSCRIPTEN_BINDINGS(TemplateClass)
+{
 
   // some other bindings here
-  
+
   emscripten::register_vector<float>("VectorFloat");
-  
+
   emscripten::class_<TemplateClass<float>>("TemplateClassFloat")
     .constructor<>()
     .property("Data", &TemplateClass<float>::getData, &TemplateClass<float>::setData);
-  //    .property("VectorData", 
+  //    .property("VectorData",
 }
