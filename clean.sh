dedbfc1ce405b3f896aa68ffe40d7d6db59c6028
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2025-02-03 21:32:06 +0100

    ..

--- clean.sh
+++ clean.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 find . -name build -type d | xargs -i -t rm -Rf {}
+find . -name build_wasm -type d | xargs -i -t rm -Rf {}
 find . -name node_modules -type d | xargs -i -t rm -Rf {}
+find . -name chrome-profile -type d | xargs -i -t rm -Rf {}
