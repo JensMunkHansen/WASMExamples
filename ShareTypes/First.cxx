d35fb0db2b772ec5e74e57624ce06289b50f490c
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2025-02-03 20:26:05 +0100

    ..

--- ShareTypes/First.cxx
+++ ShareTypes/First.cxx
@@ -1,6 +1,7 @@
 #include "First.h"
 #include "Types.h"
 #include "stdio.h"
+#include <emscripten.h>
 #include <emscripten/bind.h>
 
 int SomeStaticVariable = 1;
@@ -24,6 +25,23 @@ int ConsumeTest(MyData* data)
 }
 
 #ifdef CPPTYPES
+
+/*
+  To avoid sharing static content, one can write a linker script module1.ld and
+  append this linker argument '-Wl,-T,module1.ld'. The script contains
+
+SECTIONS {
+    .mydata1 0x2000 : {
+        KEEP(*(.my_global)) //
+    }
+}
+
+and the static variable should be defined with an attribute
+
+__attribute__((used, section(".mydata1")))
+int globalVar = 100;  // Placed in .mydata1
+
+*/
 
 int ConsumeTestCPP(struct WrappedMyData* data)
 {
